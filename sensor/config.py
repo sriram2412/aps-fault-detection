6c95eee41bf72837f030f706022802ed7add0ed7
sriram2412 <sriram241299@gmail.com>
2023-01-18 23:07:14 +0530

    updated transformation part

--- sensor/config.py
+++ sensor/config.py
@@ -8,7 +8,12 @@ import os
 class EnvironmentVariable:
     mongo_db_url:str = os.getenv("MONGO_DB_URL")
 
+
+TARGET_COLUMN_MAPPING = {
+    "pos":1,
+    "neg":0
+}
+
 env_var = EnvironmentVariable()
-
-
 mongo_client = pymongo.MongoClient(env_var.mongo_db_url)
+TARGET_COLUMN = "class"
